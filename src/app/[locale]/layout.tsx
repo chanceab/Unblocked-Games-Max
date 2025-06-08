f9f881c15a8ca9f4bf92de1a5836d23c80205d01
ho-shi <57794204+ho-shi@users.noreply.github.com>
2025-06-08 17:17:13 +0800

    feat: ad

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -50,6 +50,8 @@ export default function RootLayout(props: {
   return (
     <html lang={props.params.locale}>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-7175074472838458"></meta>
+
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7175074472838458"
